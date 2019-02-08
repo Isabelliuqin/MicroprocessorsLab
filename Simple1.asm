8978ba89fcd73a35ed53b4adc6f173a29039b8dc
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-08 11:58:04 +0000

    08022019 random number generator done, hidden card value

--- Simple1.asm
+++ Simple1.asm
@@ -2,9 +2,14 @@
 
 	extern	UART_Setup, UART_Transmit_Message   ; external UART subroutines
 	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
-	extern	LCD_Write_Hex, LCD_row_shift,LCD_clear,LCD_delay_x4us,LCD_delay_ms,LCD_row_return; external LCD subroutines
+	extern	LCD_Write_Hex, LCD_row_shift,LCD_clear,LCD_delay_x4us,LCD_delay_ms,LCD_row_return,LCD_sq; external LCD subroutines
 	extern  ADC_Setup, ADC_Read		    ; external ADC routines
 	extern	multip
+	extern	counter_setup, table_setup, counter_pickvalue
+	
+acs0	udata_acs	
+card_set1	res 10  ;player's cards
+card_set2	res 10  ;dealer's cards
 	
 rst	code	0    ; reset vector
 	goto	setup
@@ -14,30 +19,32 @@ main	code
 	; ******* Programme FLASH read Setup Code ***********************
 setup	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
+	call	counter_setup
 	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup LCD
 	call	ADC_Setup	; setup ADC
 	
-	goto	start
+	call	table_setup
+	goto	ini
 	
 	; ******* Main programme ****************************************
-start 		
-measure_loop
-	call	ADC_Read		; read in Hex data, display on LCD
-	movf	ADRESH,W
-	call	LCD_Write_Hex
-	movf	ADRESL,W
-	call	LCD_Write_Hex
-		
-	call	LCD_delay_ms
-	
-conversion
-	call	LCD_row_shift		; write to second line
-	call	multip			; convert 12 bit input to decimal output
-	
-	call	LCD_delay_ms
-	call	LCD_clear		; clear the display, ready for the next input
-	
-	goto	measure_loop		; goto current line in code
+ini 		
+    movlw   .255
+    call    LCD_delay_ms
+    movlw   .100
+    call    LCD_delay_x4us
+    
+    call    counter_pickvalue
+    movwf   card_set2		;dealer's card 1
+    
+    
+    call    LCD_delay_ms
+    ;call    counter_pickvalue
+    ;movwf   card_set2
+    call    LCD_sq		;hide dealer's card 2
+    call    LCD_delay_ms
 
-	end
+    
+    goto    $
+
+    end
