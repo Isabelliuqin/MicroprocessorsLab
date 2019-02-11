386f1d8e55b1596521758bc66bd4340878c82fa9
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-11 17:07:15 +0000

    11022019 5pm keypad pressing A B

--- keypad.asm
+++ keypad.asm
@@ -1,10 +1,18 @@
 #include p18f87k22.inc
+    extern  LCD_delay_x4us
 
-GLOBAL  KEYPAD_ini_TABLE, KEYPAD_Setup, KEYPAD_loop
+    global  KEYPAD_ini_TABLE, KEYPAD_Setup, KEYPAD_loop, Input, button_data
 acs0	udata_acs   	 ; reserve data space in access ram
+nonpressed  res 1
+  
 TABLE	udata	0x100    ; reserve data anywhere in RAM (here at 0x400)
 myTABLE res 0x0F    	 ; reserve 16 bits for message data
 
+buttondata  udata   0x120
+button_data res 1
+
+
+ 
 keypad	code
 
 KEYPAD_Setup
@@ -30,36 +38,55 @@ Input;set 0-3 as 1
     movlw   .12			; wait 48us
     call    LCD_delay_x4us
 
-    mof    PORTE,W
+    movf    PORTE,W		; combine 8 bits
     iorwf   LATH,1,0
-    call    loop
+        
+    call    KEYPAD_loop
     return
 
 KEYPAD_ini_TABLE
     lfsr    FSR0, myTABLE	; Load FSR0 with address in RAM	
+    MOVLW    b'01111110'	; input A
+    MOVWF   POSTINC0
+    MOVLW    b'01111011'	; input B
+    MOVWF   POSTINC0
+    
+    movlw   b'11111111'
+    movwf   nonpressed
+    
     MOVLW    b'01111101'	; input 0
     MOVWF   POSTINC0
     MOVLW    b'11101110'	; input 1
     MOVWF   POSTINC0
-    MOVLW    b'11010111'	; input 2
+    MOVLW    b'11101101'	; input 2
     MOVWF   POSTINC0
-    MOVLW    b'10110111'	; input 3
+    MOVLW    b'11101011'	; input 3
     RETURN
 
 KEYPAD_loop				; 3 different outputs if different buttons are pressed
-    loop0
-	movf    PORTH,W
-	lfsr    FSR2, myTABLE	; Load FSR2 with address in RAM
-	CPFSEQ  POSTINC2
-	goto    loop1
-	call	one
-	RETURN
-    loop1
-	CPFSEQ  POSTINC2
-	goto    loop2
-	call	two
-	RETURN
-    loop2
-	CPFSEQ  POSTINC2
-	goto    loop3
-	MOVLW   three
+loopA
+    movf    LATH,W
+    lfsr    FSR2, myTABLE	; Load FSR2 with address in RAM
+    CPFSEQ  POSTINC2
+    goto    loopB
+    
+    ;call    wait1
+    movlw   0x01
+    RETURN
+loopB
+    
+
+    movlw   0x02
+    movwf   button_data
+    RETURN
+
+    
+;wait1
+    ;call    Input
+    ;cpfseq  nonpressed
+    ;goto    wait1
+    ;return    
+
+
+    
+    end
