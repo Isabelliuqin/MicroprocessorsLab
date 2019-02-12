d94e8e42f51fc981d76cb47d80893adc623ca99c
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-12 11:42:20 +0000

    12022019 11.40 start the game b pressing A

--- Simple1.asm
+++ Simple1.asm
@@ -1,15 +1,15 @@
 	#include p18f87k22.inc
 
 	extern	UART_Setup, UART_Transmit_Message   ; external UART subroutines
-	extern	KEYPAD_Setup, KEYPAD_ini_TABLE, Input
+	extern	KEYPAD_Setup, KEYPAD_ini_TABLE, Keypad_Input,Keypad_button_ini
 	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
 	extern	LCD_Write_Hex, LCD_row_shift,LCD_clear,LCD_delay_x4us,LCD_delay_ms,LCD_row_return,LCD_sq,LCD_rightcorner,LCD_cursoroff,LCD_leftshift4; external LCD subroutines
 	extern  ADC_Setup, ADC_Read		    ; external ADC routines
 	extern	multip
 	extern	counter_setup, table_setup, counter_pickvalue
 	extern	clear_memory400_420, clear_memory450_460
-	extern	button_ini,ini_carddealer,ini_cardplayer
-	extern	title_setup
+	extern	ini_carddealer,ini_cardplayer
+	extern	title_setup, Title_press_to_start
 	
 
 	
@@ -32,14 +32,16 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	
 	call	clear_memory400_420
 	call	clear_memory450_460
-	call	button_ini
-	call	title_setup
 	
+	call	title_setup
+	call	Keypad_button_ini
 	goto	main
 
 	
 	; ******* Main programme ****************************************
 main
+	call	Title_press_to_start
+	
 	call	ini_carddealer
 	call	ini_cardplayer
 
