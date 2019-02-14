a76ec2dee998e23c39b7992653a4c5183e9e3ccd
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-14 17:12:37 +0000

    14022019

--- card.asm
+++ card.asm
@@ -1,15 +1,22 @@
 #include p18f87k22.inc
     
-    extern  LCD_delay_x4us, LCD_delay_ms, LCD_rightcorner, LCD_leftshift4
+    extern  LCD_delay_x4us, LCD_delay_ms, LCD_rightcorner, LCD_leftshift4,LCD_sq
     extern  Keypad_Input
     extern  counter_pickvalue
+    extern  Choice_hit_or_stand
+    extern  result
     global  addition_player, addition_dealer,ini_cardplayer, ini_carddealer
+   
     
 cardset1	udata	0x400	
 card_set1	res 10  ;player's cards
 	
 cardset2	udata	0x410
 card_set2	res 10  ;dealer's cards
+	
+acs0	udata_acs   ; reserve data space in access ram
+sum	res 1
+
 	
 card	code
     
@@ -43,10 +50,13 @@ addition_dealer
     addwf   PREINC1, 0		;sum up the initial player's card values and place it in W
     addwf   PREINC1, 0		;sum up the initial player's card values and place it in W
     addwf   PREINC1, 0		;sum up the initial player's card values and place it in W
-
+    
+    call    result
     return
+    
 
 
+    
     
 ini_carddealer
 drawcard_dealer
@@ -68,11 +78,11 @@ hiddencard_dealer
     cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
     goto    hiddencard_dealer
     
-    call    counter_pickvalue
-    movwf   POSTINC2
-    ;call    LCD_sq		;hide dealer's card 2
+    ;call    counter_pickvalue
+    ;movwf   POSTINC2
+    call    LCD_sq		;hide dealer's card 2
     ;call    LCD_delay_ms
-    call    addition_dealer
+    ;call    addition_dealer
     movlw   .255
     call    LCD_delay_ms
     
@@ -107,6 +117,7 @@ carddraw_player
     call    LCD_delay_ms
     
     call    addition_player	;sum up initial player's card values 
+    
     goto    $
     return
     
