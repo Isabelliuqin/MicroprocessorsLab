aacf4b64d0b9029c81c723c4ef7e283c368cbe51
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-15 16:22:09 +0000

    15022019 modified command_setup command_make_choice

--- command.asm
+++ command.asm
@@ -1,31 +1,30 @@
 #include p18f87k22.inc
     
-global	Choice_hit_or_stand	
-extern	Cursormove
-    acs0	udata_acs   ; reserve data space in access ram
-    counter	    res 1   ; reserve one byte for a counter variable
-    delay_count res 1   ; reserve one byte for counter in the delay routine
+    global	Command_setup, Command_make_choice,command_start	
+    extern	Cursor_move
+    extern	LCD_Write_Message, LCD_row_shift,LCD_clear_display,LCD_delay_ms,LCD_delay_x4us
+    extern	Keypad_Input
+    extern	Title_press_to_start
+acs0	udata_acs   ; reserve data space in access ram
+counter	    res 1   ; reserve one byte for a counter variable
+delay_count res 1   ; reserve one byte for counter in the delay routine
 
 
-hit_data	udata	0x600    ; reserve data anywhere in RAM (here at 0x400)
-hit_Array res 0x100    ; reserve 64 bytes for message data
+hit_data    udata   0x350    ; reserve data anywhere in RAM 
+hit_Array	res 0x50    ; reserve 108 bytes for message data
 
 
 pdata	code    ; a section of programme memory for storing data
 
 	; ******* myTable, data in programme memory, and its length *****
 hit	data	    "Hit?\n"	; message, plus carriage return
-	constant    hit_c=.4	; length of data
+	constant    hit_c=.5	; length of data
 choice1	data	    "Yes No ...\n"	; message, plus carriage return
-	constant    choice1_c =.10	; length of data
+	constant    choice1_c =.11	; length of data
 
-choice2	data	    "Double Down\n"	; message, plus carriage return
-	constant    choice2_c =.11	; length of data
-	
-choice3	data	    "Surrender\n"	; message, plus carriage return
-	constant    choice3_c =.9	; length of data
+
 	; ******* Programme FLASH read Setup Code ***********************
-hit_setup	
+Command_setup	
 	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
 	return
@@ -34,7 +33,7 @@ hit_setup
 	
 	; HIT------ -------
 command_start 	
-	call	LCD_clear	; clear LCD screen before choice page
+	call	LCD_clear_display	; clear LCD screen before choice page
 	
 	lfsr	FSR0, hit_Array	; Load FSR0 with address in RAM	
 	movlw	upper(hit)	; address of data in PM
@@ -45,20 +44,23 @@ command_start
 	movwf	TBLPTRL		; load low byte to TBLPTRL
 	movlw	hit_c		; bytes to read
 	movwf 	counter		; our counter register
-loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+loop1 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
 	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
+	bra	loop1		; keep going until finished
 		
+
 	movlw	hit_c-1	; output message to LCD (leave out "\n")
 	lfsr	FSR2, hit_Array
-	
 	;movlw	b'10000010'	;set the string to the middle of LCD
 	;call LCD_Move_to_position 
 	call	LCD_Write_Message
 	
-	call	LCD_row_shift
+	;movlw	.255		; wait 255ms
+	;call	LCD_delay_ms
 	
+	call	LCD_row_shift
+
 	; YES NO NEXT PAGE -------------
 	lfsr	FSR0, hit_Array	; Load FSR0 with address in RAM	
 	movlw	upper(choice1)	; address of data in PM
@@ -77,71 +79,22 @@ loop2 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movlw	choice1_c-1	; output message to LCD (leave out "\n")
 	lfsr	FSR2, hit_Array
 	call	LCD_Write_Message
-
+	movlw	.255		; wait 255ms
+	call	LCD_delay_ms	
 	
 	
-	; SECOND CHOICE PAGE DOUBLE DOWN -------
-	
-	lfsr	FSR0, hit_Array	; Load FSR0 with address in RAM	
-	movlw	upper(choice2)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(choice2)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(choice2)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	choice2_c		; bytes to read
-	movwf 	counter		; our counter register
-loop3 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop2		; keep going until finished
-		
-	movlw	choice2_c-1	; output message to LCD (leave out "\n")
-	lfsr	FSR2, hit_Array
-	call	LCD_Write_Message
 	
 	
-	; SECOND CHOICE PAGE SURRENDER -------
-	
-	lfsr	FSR0, hit_Array	; Load FSR0 with address in RAM	
-	movlw	upper(choice3)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(choice3)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(choice3)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	choice3_c		; bytes to read
-	movwf 	counter		; our counter register
-loop4 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop2		; keep going until finished
-		
-	movlw	choice3_c-1	; output message to LCD (leave out "\n")
-	lfsr	FSR2, hit_Array
-	call	LCD_Write_Message
-
-
 	return
 	
-Choice_hit_or_stand
+Command_make_choice
 
-	call	Keypad_Input	; let the key loop going and set W register to corresponding number
-	call	Cursormove	
 	
-	movlb   5
-	cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
-	;goto	ini_carddealer
-	goto    Title_press_to_start
-	
-	call	LCD_clear_display
-	movlw   .255
-	call    LCD_delay_ms
+	call	Cursor_move	
 	
 	
-	;goto	bet_page
 	
 	return
 
 
-end
+	end
