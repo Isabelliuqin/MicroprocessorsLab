aee760fe8a18fdd4b25e00070675873e9f40814e
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-15 16:23:53 +0000

    15022019 new file

--- recovery.asm
+++ recovery.asm
@@ -1,7 +1,7 @@
 #include p18f87k22.inc
 
     
-extern	LCD_rightcorner, LCD_Send_Byte_D, LCD_leftshift4, LCD_rightshift
+extern	LCD_rightcorner, LCD_Send_Byte_D, LCD_leftshift4, LCD_rightshift,LCD_clear_display
     
 recovery_data	udata_acs   ; reserve data space in access ram
 nocard		res 1
@@ -19,7 +19,7 @@ Recovery_card
     
 Recovery_dealer    
     movlb   4
-    lfsr    FSR0, 0x410, BANKED
+    lfsr    FSR0, 0x410
     
 Recovery_loop_dealer    
     movf    POSTINC0, W
@@ -31,7 +31,7 @@ Recovery_loop_dealer
 
 Recovery_player
     movlb   4
-    lfsr    FSR2, 0x400, BANKED
+    lfsr    FSR2, 0x400
     call    LCD_rightcorner
     
 Recovery_loop_player
