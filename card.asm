de04f4be018044072babccc9bc8aa390eadb59df
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-15 16:21:16 +0000

    15022019 modified addition player and addition dealer add drawcard dealer after player subroutine

--- card.asm
+++ card.asm
@@ -3,9 +3,9 @@
     extern  LCD_delay_x4us, LCD_delay_ms, LCD_rightcorner, LCD_leftshift4,LCD_sq
     extern  Keypad_Input
     extern  counter_pickvalue
-    extern  Choice_hit_or_stand
-    extern  result
-    global  addition_player, addition_dealer,ini_cardplayer, ini_carddealer
+    extern  Command_make_choice
+    ;extern  result
+    global  addition_player, addition_dealer,ini_cardplayer, ini_carddealer, carddraw_player, drawcard_dealer_after_player 
    
     
 cardset1	udata	0x400	
@@ -14,9 +14,16 @@ card_set1	res 10  ;player's cards
 cardset2	udata	0x410
 card_set2	res 10  ;dealer's cards
 	
+player_sum_	udata	0x420
+player_sum		res 1
+
+dealer_sum_	udata	0x430
+dealer_sum		res 1
+	
 acs0	udata_acs   ; reserve data space in access ram
 sum	res 1
 
+	
 	
 card	code
     
@@ -35,6 +42,8 @@ addition_player
     addwf   PREINC1, 0		;sum up the initial player's card values and place it in W
     addwf   PREINC1, 0		;sum up the initial player's card values and place it in W
     
+    movwf   player_sum
+    ;call    Result_before_dealerdrawcards
     return
     
 addition_dealer
@@ -51,7 +60,8 @@ addition_dealer
     addwf   PREINC1, 0		;sum up the initial player's card values and place it in W
     addwf   PREINC1, 0		;sum up the initial player's card values and place it in W
     
-    call    result
+    movwf    dealer_sum
+    ;call    Result_after_dealerdrawcards
     return
     
 
@@ -59,11 +69,11 @@ addition_dealer
     
     
 ini_carddealer
-drawcard_dealer
+drawcard_dealer_ini			;dealer's first card
     call    Keypad_Input		;detect the key pressing on keypad
     movlb   5
     cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
-    goto    drawcard_dealer
+    goto    drawcard_dealer_ini
     call    counter_pickvalue	;if A is pressed, pick the first value
     lfsr    FSR2,card_set2
     movwf   POSTINC2		;place player's card 1 in cardset1 and move to the next memory location 
@@ -71,24 +81,35 @@ drawcard_dealer
     movlw   .255
     call    LCD_delay_ms
 
-hiddencard_dealer
+hiddencard_dealer		;dealer's hidden card	
     
     call    Keypad_Input
     movlb   5
     cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
     goto    hiddencard_dealer
     
-    ;call    counter_pickvalue
-    ;movwf   POSTINC2
+
     call    LCD_sq		;hide dealer's card 2
-    ;call    LCD_delay_ms
-    ;call    addition_dealer
+
     movlw   .255
     call    LCD_delay_ms
     
     return
+
+drawcard_dealer_after_player	    ; reveal the hidden card plus 3rd 4th... card
+    call    Keypad_Input		;detect the key pressing on keypad
+    movlb   5
+    cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
+    goto    drawcard_dealer_after_player
+    call    counter_pickvalue	;if A is pressed, pick the first value
+    movlb   4
+    lfsr    FSR2,0x412
+    movwf   POSTINC2		;place player's card 1 in cardset1 and move to the next memory location 
     
+    movlw   .255
+    call    LCD_delay_ms
     
+    call    addition_dealer
     
 ini_cardplayer
     call    Keypad_Input
@@ -118,7 +139,7 @@ carddraw_player
     
     call    addition_player	;sum up initial player's card values 
     
-    goto    $
+    ;goto    $
     return
     
 
