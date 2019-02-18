07f4fb55d6212f86396837b7c3710e2c434a31c1
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-18 17:04:51 +0000

    18022019

--- Simple1.asm
+++ Simple1.asm
@@ -1,11 +1,11 @@
 	#include p18f87k22.inc
 
-	extern	UART_Setup, UART_Transmit_Message   ; external UART subroutines
+	
 	extern	KEYPAD_Setup, KEYPAD_ini_TABLE, Keypad_Input,Keypad_button_ini
 	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
 	extern	LCD_Write_Hex, LCD_row_shift,LCD_delay_x4us,LCD_delay_ms,LCD_row_return,LCD_sq,LCD_rightcorner,LCD_cursoroff,LCD_leftshift4; external LCD subroutines
-	extern  ADC_Setup, ADC_Read		    ; external ADC routines
-	extern	multip
+	
+	
 	extern	counter_setup, table_setup, counter_pickvalue
 	extern	clear_memory400_420, clear_memory450_460
 	extern	ini_carddealer,ini_cardplayer
@@ -25,9 +25,8 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	call	counter_setup
 	call	KEYPAD_Setup
 	call	KEYPAD_ini_TABLE
-	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup LCD
-	call	ADC_Setup	; setup ADC
+	
 	;call	LCD_cursoroff	; turn the cursor of LCD display off
 	call	table_setup
 	
