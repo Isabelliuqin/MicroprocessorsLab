380a21370124fa66ed76eb45c2cea20074fe7cfc
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-18 11:37:04 +0000

    18022019	move cursor goto other page

--- cursor.asm
+++ cursor.asm
@@ -16,30 +16,26 @@ Cursor_confirm
 	
 Cursor_move
 	call	LCD_row_shift
-		
+	
+	call	Keypad_Input	    ; let the key loop going and set W register to corresponding number	
 
-Check_leftshift
-	call	Keypad_Input	    ; let the key loop going and set W register to corresponding number
+Check_leftshift			    ; choose YES
+	
 	movlb	5
 	cpfseq	0x504,BANKED
 	goto    Check_rightshift
 	
 	call	LCD_leftshift
 	call	LCD_leftshift
+	call	LCD_leftshift
+	call	LCD_leftshift
 	
-	movlw   .255
-	call    LCD_delay_ms
-	
-	call    Keypad_Input	;detect the key pressing on keypad
-	movlb   5
-	cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
-	goto    Check_leftshift
-	
-	
-	goto	carddraw_player
-	
+	call	Check_nonpressed_after_pressing
+	call	Check_confirm
 	
 
+	goto	carddraw_player
+	
 	return
 	
 
@@ -53,13 +49,10 @@ Check_rightshift		;check whether 6 is preesed-shift to right
 	call	LCD_rightshift
 	call	LCD_rightshift
 	call	LCD_rightshift
-	
-	call    Keypad_Input		;detect the key pressing on keypad
-	movlb   5
-	cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
-	goto    Check_leftshift
-	call	LCD_clear_display
-	goto	drawcard_dealer_after_player	
+
+	call	Check_nonpressed_after_pressing
+	call	Check_confirm
+	goto	drawcard_dealer_after_player	    ; user confirmed to stand	
 	
 	movlw   .255
 	call    LCD_delay_ms
@@ -68,19 +61,39 @@ Check_rightshift		;check whether 6 is preesed-shift to right
 
 Check_nonpressed
 	
-	movlw	0x00
+	movlw	0x00		    
 	movlb	7
-	cpfseq	0x700, BANKED
+	cpfseq	0x700, BANKED		;compare Keypad_Input with 0x00
 	goto	Check_leftshift
 	
 	call    Keypad_Input		;detect the key pressing on keypad
 	movlb   5
 	cpfseq  0x500, BANKED	;check whether is button A being pressed, if not detect again
 	goto    Check_leftshift
+	call	LCD_clear_display
 	goto	carddraw_player
 	
 	movlw   .255
 	call    LCD_delay_ms
+	
+	return
+	
+	
+Check_nonpressed_after_pressing		;pressed the key to move cursor, check whether the user stop pressing and ready to press key A to confirm
+	call    Keypad_Input
+	movlw	0x00		    
+	movlb	7
+	cpfseq	0x700, BANKED		;equal for non pressed
+	goto	Check_nonpressed_after_pressing	
+	goto	Check_confirm
+	return
+	
+Check_confirm	
+	call    Keypad_Input		;detect confirmation by pressing A
+	movlb   5
+	cpfseq  0x500, BANKED		;check whether is button A being pressed, if not detect again
+	goto    Check_leftshift		;check whether the cursor is moved again
+	call	LCD_clear_display
 	
 	return
 	
