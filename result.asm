9fa7f117674c6c77201339a361b4e5183adfcb3c
ql1616 <ql1616@skbl-422-007.ph.ic.ac.uk>
2019-02-20 16:04:20 +0000

    20022019 recovery and result debugged

--- result.asm
+++ result.asm
@@ -1,5 +1,10 @@
 #include p18f87k22.inc
-    
+
+	extern	command_start, Command_make_choice
+	extern	drawcard_dealer_after_player
+	extern	LCD_Write_Message, LCD_delay_ms, LCD_clear_display
+	extern	Simple_player_no
+	global	Result_before_dealerdrawcards, Result_after_dealerdrawcards,Result_setup
 result_data	udata_acs
 twenty_one	res 1
 seventeen	res 1
@@ -25,67 +30,69 @@ Resultpush
 Result_setup	
 	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
-	return
 	
-result_value			;!!!!
+;result_value			;!!!!
 	movlw	0x15
 	movwf	twenty_one
 	movlw	0x11
 	movwf	seventeen
-	
+	return
 
 Result_before_dealerdrawcards			; game result totally depends on player's card
 equal_to_21
+	movlb	4
+	movf	0x420, W, BANKED
 	cpfseq	twenty_one
-	;goto	larger_than_21
-	;goto	Result_win
+	goto	not_21
+	goto	Result_win
 						;called by addition_player
 	
-larger_than_21	
+not_21	
 	cpfslt	twenty_one
-	;goto	smaller_than_21
-	;goto	Result_lose
+	goto	smaller_than_21
+	goto	Result_lose
 	
 smaller_than_21
-	;goto	command_start
-	;goto	Command_make_choice
+	
 	return
 	
 Result_after_dealerdrawcards
 	;called by addition_dealer
-;equal_to_21
+equal_to_21_a
+	movlb	4
+	movf	0x430, W, BANKED
 	cpfseq	twenty_one
-	;goto	larger_than_21
-	;goto	Result_lose
-;larger_than_21
+	goto	not_21_a
+	goto	Result_lose
+not_21_a
 	cpfslt	twenty_one
-	;goto	smaller_than_21
-	;goto	Result_win
-;smaller_than_21
-smaller_than_17
+	goto	smaller_than_21_a
+	goto	Result_win
+smaller_than_21_a
+smaller_than_17_a
 	cpfsgt	seventeen
-	;goto	larger_or_equal_to_17
-	;goto	drawcard_dealer_after_player
-	return
-larger_or_equal_to_17
-equal_to_player
+	goto	larger_or_equal_to_17_a
+	call	drawcard_dealer_after_player
+	goto	Simple_player_no
+larger_or_equal_to_17_a
+equal_to_player_a
 	movlb	4
 	cpfseq	0x420, BANKED		;compare with player's card sum, equal goto result push
-	;goto	smaller_than_player
-	;goto	Result_push
+	goto	not_equal_to_player_a
+	goto	Result_push
 	
-smaller_than_player
+not_equal_to_player_a
 	movlb	4
 	cpfsgt	0x420, BANKED
-	;goto	larger_than_player
-	;goto	Result_win
+	goto	larger_than_player
+	goto	Result_win
 	
 larger_than_player
-	;goto	Result_lose
+	goto	Result_lose
 	
 Result_win
 win_start 	
-;	call	LCD_clear_display	; clear LCD screen before choice page
+	call	LCD_clear_display	; clear LCD screen before choice page
 	
 	lfsr	FSR0, result_Array	; Load FSR0 with address in RAM	
 	movlw	upper(Resultwin)	; address of data in PM
@@ -106,13 +113,13 @@ win_loop
 	movlw	win_c-1	; output message to LCD (leave out "\n")
 	movlb	8
 	lfsr	FSR2, result_Array
-;	call	LCD_Write_Message
+	call	LCD_Write_Message
 	movlw	.255		; wait 255ms
-;	call	LCD_delay_ms
-	return
+	call	LCD_delay_ms
+	goto	$
 Result_lose
 lose_start 	
-;	call	LCD_clear_display	; clear LCD screen before choice page
+	call	LCD_clear_display	; clear LCD screen before choice page
 	
 	lfsr	FSR0, result_Array	; Load FSR0 with address in RAM	
 	movlw	upper(Resultlose)	; address of data in PM
@@ -132,13 +139,14 @@ lose_loop
 	movlw	lose_c-1	; output message to LCD (leave out "\n")
 	movlb	8
 	lfsr	FSR2, result_Array
-	;call	LCD_Write_Message
+	call	LCD_Write_Message
 	movlw	.255		; wait 255ms
-	;call	LCD_delay_ms
-	return
-	
+	call	LCD_delay_ms
+	goto	$
+	;goto	restart
+Result_push	
 push_start 	
-	;call	LCD_clear_display	; clear LCD screen before choice page
+	call	LCD_clear_display	; clear LCD screen before choice page
 	
 	lfsr	FSR0, result_Array	; Load FSR0 with address in RAM	
 	movlw	upper(Resultpush)	; address of data in PM
@@ -159,8 +167,8 @@ push_loop
 	movlw	win_c-1	; output message to LCD (leave out "\n")
 	movlb	8
 	lfsr	FSR2, result_Array
-	;call	LCD_Write_Message
+	call	LCD_Write_Message
 	movlw	.255		; wait 255ms
-	;call	LCD_delay_ms
-	return
+	call	LCD_delay_ms
+	goto	$
     end
